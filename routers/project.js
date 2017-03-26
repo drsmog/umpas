31d959270999c10933305099d39418bb800c1357
irakli svanidze <irakli1393@gmail.com>
2017-03-26 20:43:26 +0400

    linting

--- routers/project.js
+++ routers/project.js
@@ -1,37 +1,47 @@
 const router = require('express').Router();
-const logger = require('../utils/logger').getLogger('app');
+// const logger = require('../utils/logger').getLogger('app');
 
 router.get('/', function(req, res, next) {
+
     let projects = [{
             id: 1,
             name: 'dayCenter',
-            description: 'baby care organizations'
-        },
-        {
+            description: 'baby care organizations',
+        }, {
             id: 2,
             name: 'high mountine',
-            description: 'Benefits for village doctors'
-        },
-        {
+            description: 'Benefits for village doctors',
+        }, {
             id: 3,
             name: 'Help Desk',
-            description: 'ehealth internal helpdesk system'
+            description: 'ehealth internal helpdesk system',
         },
 
     ];
 
+
     return res.send({
         success: true,
-        data: projects
+        data: projects,
     });
+
 });
 
-router.post('/',function (req,res,next) {
-  return res.send({success:true,data:{id:5}});
+router.post('/', function(req, res, next) {
+
+  return res.send({success: true, data: {id: 5}});
+
 });
 
-router.put('/',function (req,res,next) {
-  return res.send({success:true,data:{}});
+router.put('/', function(req, res, next) {
+
+  return res.send({success: true, data: {}});
+
+});
+
+router.get('/sdf', function(req, res, next) {
+
+
 });
 
 module.exports = router;
