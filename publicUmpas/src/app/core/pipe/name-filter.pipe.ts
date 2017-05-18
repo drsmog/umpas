9341f6d172774fee76889b6a6204b18b26feaf62
irakli jiishkariani <irakli2692@gmail.com>
2017-05-18 17:41:49 +0400

    generalize name-filter.pipe.ts
    add function parameter, which returns item's name, and use it in filter

--- publicUmpas/src/app/core/pipe/name-filter.pipe.ts
+++ publicUmpas/src/app/core/pipe/name-filter.pipe.ts
@@ -5,8 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: false
 })
 export class NameFilterPipe implements PipeTransform {
-  transform(items: any[], name: any): any {
-    return items.filter((item) => item.name.includes(name));
+  nameFieldDefault = item => item.name;
+
+  transform(items: any[], name: any, getNameField?: (item: any) => string): any {
+    if (!getNameField) {
+      getNameField = this.nameFieldDefault;
+    }
+
+    return items.filter((item) => getNameField(item).includes(name));
   }
 
 }
