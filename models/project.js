978c37703c36f193feab6360fbb7a9a20a1e5890
irakli jiishkariani <irakli2692@gmail.com>
2017-06-13 11:34:16 +0400

    use bluebird promise on mongoose

--- models/project.js
+++ models/project.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Promise = require('bluebird');
+
+Promise.config({warnings: false});
+
+mongoose.Promise = Promise;
+
 const ProjectSchema = new Schema({
   name: String,
   description: String,
