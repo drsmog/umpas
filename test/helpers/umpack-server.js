71e47ed53eb90f8203a6e0176784f825c38c658d
irakli jiishkariani <irakli2692@gmail.com>
2017-06-16 15:48:21 +0400

    test environment

---
+++ test/helpers/umpack-server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const config = require('config');
+const bodyParser = require('body-parser');
+const umpack = require('umpack-express');
+
+
+const app = express();
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/um', umpack.router);
+
+
+app.listen(config.get('umpackServer.port'), function() {
+    // console.log('umpack start listening');
+});
