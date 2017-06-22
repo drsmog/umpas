bc0cfd636836a6d884ee5757e5ef2434576bd134
irakli jiishkariani <irakli2692@gmail.com>
2017-06-22 12:46:34 +0400

    project-db-hooks helper add, add mongoDatabase field in test.json

---
+++ test/helpers/project-db-hooks.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const projectsCollection = 'projects';
+const rootUser = 'root';
+const password = '123456';
+
+exports.truncateProjectsCollection = function () {
+  return mongoose.connection.db.collection(projectsCollection).remove();
+};
+
+exports.insertTestProject = function () {
+  return mongoose.connection.db.collection(projectsCollection)
+    .insert({
+      name: 'test project',
+      url: 'localhost:' + config.get('umpackServer.port'),
+      username: rootUser,
+      password: password,
+      umBaseUrl: '/um'
+    });
+};
