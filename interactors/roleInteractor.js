7d037b2818b28c0012c1f4b5a10d634544b461cf
irakli jiishkariani <irakli2692@gmail.com>
2017-06-28 18:37:36 +0400

    roleInteractor getFullRoles

---
+++ interactors/roleInteractor.js
@@ -0,0 +1,12 @@
+const Promise = require('bluebird');
+const _ = require('lodash');
+const projectInteractor = require('./projectInteractor');
+
+exports.getFullRoles = function (projectId) {
+  return projectInteractor.getLoggedInProjectService(projectId)
+    .then(function (service) {
+      return Promise.map(service.getRoles(), function (role) {
+        return service.getFullRole(role);
+      });
+    });
+};
