81a6a455bd5200baf91daba5366460b89845a977
irakli jiishkariani <irakli2692@gmail.com>
2017-06-29 15:34:08 +0400

    create role with actions backend

--- interactors/roleInteractor.js
+++ interactors/roleInteractor.js
@@ -2,18 +2,38 @@ const Promise = require('bluebird');
 const _ = require('lodash');
 const projectInteractor = require('./projectInteractor');
 
-exports.getFullRoles = function (projectId) {
+exports.getFullRoles = function(projectId) {
   return projectInteractor.getLoggedInProjectService(projectId)
-    .then(function (service) {
-      return Promise.map(service.getRoles(), function (role) {
+    .then(function(service) {
+      return Promise.map(service.getRoles(), function(role) {
         return service.getFullRole(role);
       });
     });
 };
 
-exports.deleteRole = function (projectId, role) {
+exports.deleteRole = function(projectId, role) {
   return projectInteractor.getLoggedInProjectService(projectId)
-    .then(function (service) {
+    .then(function(service) {
       return service.deleteRole(role);
+    });
+};
+
+exports.createRoleWithActions = function(projectId, roleObject) {
+  return projectInteractor.getLoggedInProjectService(projectId)
+    .then(function(service) {
+      return service.createRole(roleObject.name)
+        .then(function() {
+          return Promise.map(roleObject.actions, function(action) {
+            return service.permitActionToRole(roleObject.name, action)
+              .then(function(result) {
+                return Object.assign({}, action, result.actionId);
+              });
+          });
+        });
+    })
+    .then(function(actions) {
+      roleObject.actions = actions;
+
+      return roleObject;
     });
 };
