216486fa741bc1eeb76b9bccbeaa4998ebb30e28
irakli jiishkariani <irakli2692@gmail.com>
2017-06-30 11:53:27 +0400

    fix getFullRoles bug
    pass role name to getFullRole instead of role object with name field

--- interactors/roleInteractor.js
+++ interactors/roleInteractor.js
@@ -5,8 +5,8 @@ const projectInteractor = require('./projectInteractor');
 exports.getFullRoles = function(projectId) {
   return projectInteractor.getLoggedInProjectService(projectId)
     .then(function(service) {
-      return Promise.map(service.getRoles(), function(role) {
-        return service.getFullRole(role);
+      return Promise.map(service.getRoles(), function(roleObject) {
+        return service.getFullRole(roleObject.name);
       });
     });
 };
